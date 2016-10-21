855ea922e96fd4cec73713fb5219cc8a1a7eca1b
dingkple <king.kple@gmail.com>
2016-10-21 19:10:26 -0400

    add reducer

---
+++ homework3_pagerank/pagerank/src/main/java/MapReduce/PrepareReducer.java
@@ -0,0 +1,42 @@
+package MapReduce;
+
+import Pagerank.RunPagerank;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+/**
+ * Created by kingkz on 10/21/16.
+ */
+public class PrepareReducer extends Reducer<LinkPoint, LinkPointArrayWritable, LinkPoint, LinkPointArrayWritable> {
+
+    private Counter danglingCounter;
+    private Counter linkCounter;
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        danglingCounter = context.getCounter(RunPagerank.UpdateCounter.NUMBER_OF_DANGLING);
+        linkCounter = context.getCounter(RunPagerank.UpdateCounter.NUMBER_OF_RECORD);
+    }
+
+    @Override
+    protected void reduce(LinkPoint key, Iterable<LinkPointArrayWritable> values, Context context) throws IOException, InterruptedException {
+        HashSet<LinkPoint> outlinks = new HashSet<>();
+
+        linkCounter.increment(1);
+        for (LinkPointArrayWritable w : values) {
+            for (Writable o : w.get()) {
+                outlinks.add(new LinkPoint((LinkPoint)o));
+            }
+        }
+
+        if (outlinks.size() == 0) {
+            danglingCounter.increment(1);
+        } else {
+            context.write(key, new LinkPointArrayWritable(outlinks));
+        }
+    }
+}
